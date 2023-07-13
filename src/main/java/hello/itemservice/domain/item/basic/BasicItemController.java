258331c943551488dde679c5c72b275613daa898
spike1574 <spike1574@naver.com>
2023-07-13 17:49:32 +0900

    71강

--- src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -92,12 +93,29 @@ public class BasicItemController {
     @ModelAttribute 자체 생략 가능
     @RequestParam 생략 가능 (기본타입)이면 RequestParam으로 인식
      */
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(Item item){
 
         itemRepository.save(item);
 
         return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV5(Item item){
+
+        itemRepository.save(item);
+
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+
+        Item saveItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", saveItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
